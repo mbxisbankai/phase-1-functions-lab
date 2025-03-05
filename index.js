37c36e24d155214ac89f2c2fbcb6e3053e1e3096
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-03-05 08:54:34 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    const hq = 42;
+    if (location >= hq){
+        return location - hq;
+    } else {
+        return hq - location;
+    }
+}
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+    if (end > start){
+        return (end - start) * 264;
+    } else {
+    return (start - end) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000){
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02; 
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500){
+        return 25;
+    } else {
+        return `cannot travel that far`;
+    }
+}
